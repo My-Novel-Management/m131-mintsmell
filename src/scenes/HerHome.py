de7f84f467765df34f3b1ddf7d4a2185d19823a0
nagisc007 <nagisc007@yahoo.co.jp>
2020-08-26 21:26:58 +0900

    [WIP] Add ep2 conte

--- src/scenes/HerHome.py
+++ src/scenes/HerHome.py
@@ -11,9 +11,23 @@ from storybuilder.builder.world import World
 
 ## scenes
 def kindness(w: World):
+    na, mina = w.get("natsu"), w.get("mina")
+    sis = w.get("sis")
     return w.scene('やさしい先生',
             w.cmd.change_stage("HerHome"),
             w.plot_note("当時から特に自分の意見を持たない子どもだった$natsuは、それに素直に従い、翌日には彼女の家にお邪魔した"),
             w.plot_note("$minaの教え方は丁寧で、$natsuのテストの点はすぐに上がった"),
+            na.come(),
+            mina.be(),
+            na.do("翌日、彼女の家にお邪魔した"),
+            na.do("新築のマンションの一室で、ひと部屋に大きなテーブルがあり、そこに数名の生徒がいた"),
+            na.do("紅茶はミントティーで、彼女が焼いたというクッキーが皿に盛られていた"),
+            na.do("授業は三人対$minaで行われた"),
+            na.do("一人一人に丁寧に教えてくれる"),
+            na.do("$S自身は算数も理科も国語も躓いたりはしていなかったけれど、英語に手間取っていた"),
+            na.do("違う言語を学ぶ理由がよく分からなかったからだ"),
+            na.do("あまり考えすぎずにただ言われるがままに覚えるのもいいと教えられ、とりあえず覚えてそのまま答えを書くことにした"),
+            na.do("そのお陰か英語の点数以外も成績が伸びて、両親は喜んだ"),
+            na.do("結果的に半年くらいお世話になった"),
             )
 
--- src/scenes/ParentsHome.py
+++ src/scenes/ParentsHome.py
@@ -11,6 +11,8 @@ from storybuilder.builder.world import World
 
 ## scenes
 def meet_mina(w: World):
+    na = w.get("natsu")
+    mina, sis = w.get("mina"), w.get("sis")
     return w.scene('$minaとの出会い',
             w.cmd.change_stage("ParentsHome"),
             w.plot_note("$natsuは妻にある夏の出来事を話して聞かせる"),
@@ -22,5 +24,17 @@ def meet_mina(w: World):
             w.plot_note("おしとやかで、花嫁修業をしに母方の実家で色々と習い事をしながら、",
                 "自分でも塾をして子ども相手に教えている"),
             w.plot_note("$minaは$natsuも一度体験入塾をしてほしいと言ってくる"),
+            na.be(),
+            sis.come("姉が帰ってくる"),
+            sis.talk("ただいま"),
+            mina.come(),
+            mina.talk("おじゃまします"),
+            na.do("姉が知らない人を連れてきた"),
+            na.do("とても美人でおしとやかで、お嬢様っぽくて、笑うときも口元を隠すような"),
+            na.do("白い襟付きのブラウスがよく似合っていた"),
+            sis.talk("近所で塾を始めた$minaさん",
+                "今生徒を探してるんだって",
+                "あんたどう？"),
+            na.do("そんな出会いだった"),
             )
 
